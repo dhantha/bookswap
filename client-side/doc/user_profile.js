bd269933420eff453803acf6f8b0b868e0e7e42a
edcallaghan <ejc87@drexel.edu>
2017-03-18 15:07:47 -0400

    commit

--- client-side/doc/user_profile.js
+++ client-side/doc/user_profile.js
@@ -1,44 +1,7 @@
 $(document).ready(function(){
 	var ID = decodeURIComponent(window.location.search.substring(1)); // this should be the user id
 
-	$.ajax({
-		url: "http://localhost:8080/User",
-		dataType: 'jsonp',
-		data: {id:ID},
-		error: function(){
-			// indicate there is an error
-		},
-		success: function(data){
-			$("name").innerHTML(data.name);
-			$("email").innerHTML(data.email);
-		},
-		type: 'POST'
-	});
-	// books want
-	$.ajax({
-		url: 'http://location:8080/booksWant',
-		dataType: 'jsonp',
-		data: {id:ID},
-		error:function(){
-			//some error
-		},
-		success:function(data){
-			$("Want Table").innerHTML(data);
-		}
-		type: 'POST'
-	}); 
-
-	// books have
-	$.ajax({
-		url: "http://localhost:8080/booksHave",
-		dataType: 'jsonp',
-		data: {id:ID},
-		error: function(){
-			// there is an error
-		},
-		success: function(data){
-			$("Have Table").innerHTML(data);
-		},
-		type: 'POST'
-	});
-});
+  launchPostReq('User',      updateNameEmail, ID);
+  launchPostReq('booksWant', updateBooksWant, ID);
+  launchPostReq('booksHave', updateBooksHave, ID);
+})
